e68b329fb8a14f7fa12d237b9dd180a3399e2300
petrissakov <petrissakov@gmail.com>
2022-08-13 21:27:11 +0600

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+let starts = [];
+let ends = [];
+for(config of bracketsConfig){
+  starts.push(config[0]);
+  ends.push(config[1]);
 }
+let pairs = [];
+for(char of str){
+  // console.log(char);
+  if(starts.includes(char)){
+    if(starts.indexOf(char)==ends.indexOf(char)){
+      console.log('equal')
+      if(pairs.length!=0 && pairs[pairs.length-1].indexOf(char)>=0) pairs[pairs.length - 1].push(char);
+      else  pairs.push([char]);
+    }
+    else pairs.push([char]);
+
+  } else if(ends.includes(char)){
+    if(pairs.length==0) return false;
+     pairs[pairs.length - 1].push(char);
+  } else return false;
+  for(bracket of bracketsConfig){
+    console.log(bracket.toString());
+    console.log(pairs);
+    if(pairs[pairs.length-1].toString()==bracket.toString()) {pairs.pop();break;}
+    
+  }
+  
+
+ 
+}
+console.log(pairs);
+if (pairs.length == 0) return true;
+else return false;
+
+}
+
+str = '||';
+
+config = [['|', '|']];;
+
+console.log(module.exports(str, config));
